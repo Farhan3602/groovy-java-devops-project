bf542658c42f196a9d8f254f7d9f4aeb13dd6795
Farhan Shaikh <farhanshaikh3602@gmail.com>
2026-01-16 21:47:04 +0530

    added ls cmd part

--- java-devops-project.groovy
+++ java-devops-project.groovy
@@ -8,17 +8,5 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Farhan3602/Java-DevOps-Project'
             }
         }
-        stage('List files on EC2 home directory') {
-    steps {
-        sshagent(credentials: ['your-ec2-ssh-credential-id']) {
-            sh '''
-                ssh -o StrictHostKeyChecking=no \
-                    -o UserKnownHostsFile=/dev/null \
-                    ec2-ubuntu@13.200.16.136 \
-                    "ls -la /home/ubuntu@ip-172-31-20-250/"
-                '''
-            }
-        }
-    }
     }
 }
