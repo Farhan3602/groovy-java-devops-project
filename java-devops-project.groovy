04590b38b8a1c7763c7dc29cd42f738920348435
Farhan Shaikh <farhanshaikh3602@gmail.com>
2026-01-19 21:42:45 +0530

    ls cmd

--- java-devops-project.groovy
+++ java-devops-project.groovy
@@ -6,12 +6,13 @@ pipeline {
             steps {
                 // Fetch the repository using SSH credentials
                 git branch: 'main', url: 'https://github.com/Farhan3602/Java-DevOps-Project'
+                sh "scp -r * ubuntu@43.204.147.221:/home/ubuntu"
             }
         }
         stage('List Remote Files') {
             steps {
                 sshagent(['docker-server']) {
-                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@65.0.170.92 "ls -la ~/"'
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@65.0.170.92 "docker build -t farhancool/pyapp:v1 ."'
                     }
                 }
         }
