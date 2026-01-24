423264cd8f726f5d1320b5bab7ebe8bb9f6b9052
Farhan Shaikh <farhanshaikh3602@gmail.com>
2026-01-24 21:07:50 +0530

    scp transfer

--- java-devops-project.groovy
+++ java-devops-project.groovy
@@ -6,7 +6,8 @@ pipeline {
             steps {
                 // Fetch the repository using SSH credentials
                 git branch: 'main', url: 'https://github.com/Farhan3602/Java-DevOps-Project'
-                sh "scp -r * ubuntu@65.0.170.92:/home/ubuntu"
+                //sh "scp -r * ubuntu@65.0.170.92:/home/ubuntu"
+                sh 'scp -o StrictHostKeyChecking=no -r transfer ubuntu@65.0.170.92:/home/ubuntu'
             }
         }
         stage('List Remote Files') {
