67086060582c15f3d99697fe2fe62f099b0e295b
Farhan Shaikh <farhanshaikh3602@gmail.com>
2026-01-27 20:50:08 +0530

    docker login

--- java-devops-project.groovy
+++ java-devops-project.groovy
@@ -7,22 +7,22 @@ pipeline {
                 // Fetch the repository using SSH credentials
                 git branch: 'main', url: 'https://github.com/Farhan3602/Java-DevOps-Project'
                 //sh "scp -r * ubuntu@65.0.170.92:/home/ubuntu"
-                sh 'scp -o StrictHostKeyChecking=no -r * ubuntu@15.206.164.15:/home/ubuntu'
+                sh 'scp -o StrictHostKeyChecking=no -r * ubuntu@13.201.178.164:/home/ubuntu'
             }
         }
         stage('creating a docker image') {
             steps {
                 sshagent(['docker-server']) {
-                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@15.206.164.15 "docker build -t farhancool/krish:$BUILD_ID ."'
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.178.164 "docker build -t farhancool/krish:$BUILD_ID ."'
                     }
                 }
         }
         stage('Push Image to DockerHub'){
             steps {
             withCredentials([string(credentialsId: 'farhancool', variable: 'DockerPass')]) {
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@15.206.164.15 "docker login -u farhancool -p ${DockerPass}"'
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@15.206.164.15 "docker image push farhancool/krish:$BUILD_ID"'
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@15.206.164.15 "docker image push farhancool/krish:latest"'
+                sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.178.164 "docker login -u farhancool -p ${DockerPass}"'
+                sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.178.164 "docker image push farhancool/krish:$BUILD_ID"'
+                sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.178.164 "docker image push farhancool/krish:latest"'
                 }
             }
         }
